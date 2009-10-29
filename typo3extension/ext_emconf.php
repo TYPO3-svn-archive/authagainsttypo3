c0a294ce630b90d4f2513f9800983e19459b1fad
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-29 10:44:44 +0000

    First Upload to SVN
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/authagainsttypo3/trunk@25974 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ typo3extension/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "authagainsttypo3"
+#
+# Auto generated 27-10-2009 16:53
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Auth Against TYPO3',
+	'description' => 'Provides a Webservice which can be used to Athanticate against TYPO3 FE Users',
+	'category' => 'fe',
+	'author' => 'Ingo Schmitt',
+	'author_email' => 'is@marketing-factory.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"3d90";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4be7";s:14:"ext_tables.php";s:4:"8541";s:16:"locallang_db.xml";s:4:"c608";s:19:"doc/wizard_form.dat";s:4:"3e67";s:20:"doc/wizard_form.html";s:4:"430d";s:37:"pi1/class.tx_authagainsttypo3_pi1.php";s:4:"8fc4";s:17:"pi1/locallang.xml";s:4:"bfa9";s:24:"pi1/static/editorcfg.txt";s:4:"2f67";s:31:"static/Webservice/constants.txt";s:4:"d41d";s:27:"static/Webservice/setup.txt";s:4:"d41d";}',
+);
+
+?>
