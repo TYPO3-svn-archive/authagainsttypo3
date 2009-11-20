8efebe29345489dcf394876c148d19293e0f64ce
ischmittis <ischmittis@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-20 15:07:32 +0000

    * Added Extension Icon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/authagainsttypo3/trunk@26766 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3extension/ext_emconf.php
+++ typo3extension/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "authagainsttypo3"
 #
-# Auto generated 27-10-2009 16:53
+# Auto generated 03-11-2009 14:48
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Marketing Factory Consulting GmbH',
-	'version' => '0.9.1',
+	'version' => '0.9.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"3d90";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4be7";s:14:"ext_tables.php";s:4:"8541";s:16:"locallang_db.xml";s:4:"c608";s:19:"doc/wizard_form.dat";s:4:"3e67";s:20:"doc/wizard_form.html";s:4:"430d";s:37:"pi1/class.tx_authagainsttypo3_pi1.php";s:4:"8fc4";s:17:"pi1/locallang.xml";s:4:"bfa9";s:24:"pi1/static/editorcfg.txt";s:4:"2f67";s:31:"static/Webservice/constants.txt";s:4:"d41d";s:27:"static/Webservice/setup.txt";s:4:"d41d";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"60b0";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"8f26";s:14:"ext_tables.php";s:4:"d10d";s:16:"locallang_db.xml";s:4:"c608";s:14:"doc/manual.sxw";s:4:"d615";s:37:"pi1/class.tx_authagainsttypo3_pi1.php";s:4:"b21b";s:17:"pi1/locallang.xml";s:4:"bfa9";s:24:"pi1/static/editorcfg.txt";s:4:"2f67";s:31:"static/Webservice/constants.txt";s:4:"790a";s:27:"static/Webservice/setup.txt";s:4:"3d72";s:21:"contrib/post-test.php";s:4:"6484";}',
+	'suggests' => array(
+	),
 );
 
 ?>
